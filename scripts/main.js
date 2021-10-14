408f30730e4de98fceadb65647de56728d25f011
Steve Brownlee <chortlehoort@gmail.com>
2021-10-14 11:07:46 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+import { getEmployees, getOrders, getProducts } from "./database.js";
+
+
+const ordersByEmployee = (orders, employeeId) => {}
+const ordersByProduct = (orders, productId) => {}
+const maxGrossProduct = (orders) => {}
+const topSalesperson = (orders) => {}
+const monthlyOrders = (orders, month, year) => {}
+const orderSum = (orders) => {}
+
+/*
+    Total receipts for all time
+*/
+
+/*
+    Total receipts for avocado donuts (all time)
+*/
+
+/*
+    Top salesperson of all time
+*/
+
+
+/*
+    Top salesperson for this month
+*/
+
+/*
+    Top salesperson for last month
+*/
+
+/*
+    Milk chocolate donuts sold in a particular month
+*/
+
+/*
+    Total profit for milk chocolate donuts sold in a particular month
+*/
+
+/*
+    Top grossing product for January
+*/
